d55e87dd0c0e8181ef9a69afbc799fd3e3faff73
AtakTalay <40243456+AtakTalay@users.noreply.github.com>
2023-05-26 17:16:23 +0300

    View Ad Details

--- frontend/src/app/view-ad-details/view-ad-details.component.ts
+++ frontend/src/app/view-ad-details/view-ad-details.component.ts
@@ -8,6 +8,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ViewAdDetailsComponent implements OnInit {
   id: string;
+  text: string = `
+  Responsibilities 
+Work on data-driven projects from start to finish while meeting technical and creative requirements. 
+Collaborate with data scientists, engineers, and product managers to iterate rapidly and deliver innovative solutions.
+Analyze data using statistical methods to derive insights and make data-driven decisions.
+Work with large datasets and develop algorithms for predictive modeling, data classification, and data clustering.
+Participate in regular data science reviews and other team-wide data science efforts; contribute to a great data science team culture.
+Stay up-to-date with the latest data science technologies and techniques, and share knowledge with team members.
+Document data science workflows and share best practices with team members.
+  
+  `;
+
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
